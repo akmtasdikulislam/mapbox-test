259a583c7e7c1fa4c60063038751e4eef3a0fd99
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2026-01-08 19:16:31 +0600

    Project Initialized: Installed tailwindCSS, basicSSL, MapBox GL JS

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    tailwindcss(),
+    basicSsl(),
   ],
-})
+  server: {
+    https: true,
+  },
+});
